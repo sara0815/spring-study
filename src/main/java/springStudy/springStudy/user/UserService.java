9f3b8c8961786d8ce30e4d4d406eb3a57a98fb09
sara0815 <sara815@nate.com>
2025-07-03 22:12:41 +0900

    복구

--- src/main/java/springStudy/springStudy/user/UserService.java
+++ src/main/java/springStudy/springStudy/user/UserService.java
@@ -21,31 +21,47 @@ public class UserService {
 
     public ResponseEntity<Map<String, Object>> join(User user) {
         Map<String, Object> returnObj = new HashMap<>();
-//        if (user.userName == null || user.password == null) {
-//            returnObj.put("statusCode", 400);
-//            returnObj.put("msg", "필수값 없음");
-//            return ResponseEntity.badRequest().body(returnObj);
-//        }
-        User savedUser = userRepository.save(user);
+        try {
+            if (user.userName == null || user.password == null) {
+                returnObj.put("statusCode", 400);
+                returnObj.put("msg", "필수값 없음");
+                return ResponseEntity.badRequest().body(returnObj);
+            }
+            User savedUser = userRepository.save(user);
 
-        returnObj.put("statusCode", 200);
-        returnObj.put("msg", "회원 가입 성공");
-        return ResponseEntity.ok(returnObj);
+            returnObj.put("statusCode", 200);
+            returnObj.put("msg", "회원 가입 성공");
+            return ResponseEntity.ok(returnObj);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            returnObj.put("statusCode", 500);
+            returnObj.put("msg", "서버 오류");
+            return ResponseEntity.status(500).body(returnObj);
+        }
     }
 
     public ResponseEntity<Map<String, Object>> login(String userName, String password, HttpServletResponse response) {
         Map<String, Object> returnObj = new HashMap<>();
 
-        Optional<User> loginUser = Optional.ofNullable(userRepository.findByUserName(userName));
-        if (loginUser.isEmpty() || !loginUser.get().getPassword().equals(password)) {
-            returnObj.put("msg", "아이디나 비밀번호가 일치하지 않습니다.");
-            return ResponseEntity.badRequest().body(returnObj);
-        }
-        String token = jwtUtil.createToken(userName);
-        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
+        try {
+            Optional<User> loginUser = Optional.ofNullable(userRepository.findByUserName(userName));
+            if (loginUser.isEmpty() || !loginUser.get().getPassword().equals(password)) {
+                returnObj.put("msg", "아이디나 비밀번호가 일치하지 않습니다.");
+                return ResponseEntity.badRequest().body(returnObj);
+            }
+            String token = jwtUtil.createToken(userName);
+            response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
 
-        returnObj.put("statusCode", 200);
-        returnObj.put("msg", "로그인 성공");
-        return ResponseEntity.ok(returnObj);
+            returnObj.put("statusCode", 200);
+            returnObj.put("msg", "로그인 성공");
+            return ResponseEntity.ok(returnObj);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            returnObj.put("statusCode", 500);
+            returnObj.put("msg", "서버 오류");
+            return ResponseEntity.status(500).body(returnObj);
+        }
     }
 }
--- src/main/java/springStudy/springStudy/util/JwtUtil.java
+++ src/main/java/springStudy/springStudy/util/JwtUtil.java
@@ -1,4 +1,81 @@
 package springStudy.springStudy.util;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class JwtUtil {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String AUTHORIZATION_KEY = "auth";
+    private static final long TOKEN_EXPIRES = 1000 * 60 * 60 * 24;
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+    private Key key;
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    // header 토큰을 가져오기
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+
+    // 토큰 생성
+    public String createToken(String userName) {
+        Date date = new Date();
+        String token = Jwts.builder().setSubject(userName)
+                .setExpiration(new Date(date.getTime() + TOKEN_EXPIRES))
+                .setIssuedAt(date)
+                .signWith(key, signatureAlgorithm)
+                .compact();
+        return "Bearer " + token;
+    }
+
+    // 토큰 검증
+    // todo validateToken
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            System.out.println("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+        } catch (ExpiredJwtException e) {
+            System.out.println("Expired JWT token, 만료된 JWT token 입니다.");
+        } catch (UnsupportedJwtException e) {
+            System.out.println("Unsupported JWT token, 지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("JWT claims is empty, 잘못된 JWT 토큰입니다.");
+        } catch (Exception e) {
+            System.out.println("오류");
+        }
+        return false;
+    }
+
+    // 토큰에서 사용자 정보 가져오기
+    public Claims getUserInfoFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
 }
